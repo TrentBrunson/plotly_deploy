0775a3d744e9a6f3ca58a2bbc607e87aa44410a6
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-17 21:19:00 -0500

    added demographic info to panel

--- plots.js
+++ plots.js
@@ -12,9 +12,33 @@ function init() {
       });
   })}
   
-  init();
+  // function optionChanged(newSample) {
+  //   console.log(newSample);
+  // }
 
   function optionChanged(newSample) {
     buildMetadata(newSample);
     buildCharts(newSample);
   }
+
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+      PANEL.html("");  // clear html panel
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(key.toUpperCase() + ': ' + value);
+      })
+        // console.log(key.toUpperCase() + ': ' + value));
+    });
+  }
+
+// function buildCharts(sample){
+//   d2.json("samples.json").then(data) => {
+
+//   }
+// }
+
+  init();
