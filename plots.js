43339e5e0524935709799e55565ebb3bf691c4d5
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-17 20:15:52 -0500

    bb biome

---
+++ plots.js
@@ -0,0 +1,20 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
