59a67bd5b44928c63959c78e9ff073c2b30d1aa1
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-17 22:31:38 -0500

    h-bar and bubble charts working

--- plots.js
+++ plots.js
@@ -11,14 +11,15 @@ function init() {
           .property("value", sample);
       });
   })}
-  
+  init();
   // function optionChanged(newSample) {
   //   console.log(newSample);
   // }
 
   function optionChanged(newSample) {
     buildMetadata(newSample);
-    buildCharts(newSample);
+    buildBarChart(newSample);
+    buildBubbleChart(newSample);
   }
 
   function buildMetadata(sample) {
@@ -35,10 +36,52 @@ function init() {
     });
   }
 
-// function buildCharts(sample){
-//   d2.json("samples.json").then(data) => {
+function buildBarChart(sample){
 
-//   }
-// }
+  d3.json("samples.json").then((data) => {
+    var resultArray = data.samples.filter(sampleObj => {
+      return sampleObj.id === sample});
+    var result = resultArray[0]; // get ID number out of array
+    // store top 10 bio results for bar chart
+    var top_ten_otu_ids = result.otu_ids.slice(0,10).map(numericIds => {
+      return 'OTU ' + numericIds;}).reverse();
+    var top_ten_sample_values = result.sample_values.slice(0,10).reverse();
+    var top_ten_otu_labels = result.otu_labels.slice(0,10).reverse();
+    var bar_trace = [{
+      x : top_ten_sample_values,
+      y : top_ten_otu_ids,
+      text: top_ten_otu_labels,
+      name: "Top 10",
+      type: "bar",
+      orientation: "h"
+    }];
+    var bar_layout = {title: "Top 10 OTUs"};
+    Plotly.newPlot('bar', bar_trace, bar_layout);
+  });
+}
 
-  init();
+function buildBubbleChart(sample){
+  d3.json("samples.json").then((data) => {
+    var resultArray = data.samples.filter(sampleObj => {
+      return sampleObj.id === sample
+    });
+  var result = resultArray[0];
+  var otu_ids = result.otu_ids.map(numericIds => {
+    return numericIds;}).reverse();
+  var sample_values = result.sample_values.reverse();
+  var otu_labels = result.otu_labels.reverse();
+  var bubble_trace = [{
+    x : otu_ids,
+    y : sample_values,
+    text: otu_labels,
+    mode: 'markers',
+    marker: {color: otu_ids, size: sample_values}
+  }];
+  var bubble_layout = {
+    title: "OTU ID",
+    // showlegend: false
+  };
+  Plotly.newPlot('bubble', bubble_trace, bubble_layout)
+  });
+}
+
